f0c3c5b28010b5cc66f95c4e0c6ef3bb93dcf379
Vipul Kanade <vipultechjoy@gmail.com>
2015-10-04 14:13:56 -0700

    Updated

--- MobileApp/OpportunityFund/app/src/main/java/com/opohack/opportunityfund/paypal/view/MainActivity.java
+++ MobileApp/OpportunityFund/app/src/main/java/com/opohack/opportunityfund/paypal/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -22,6 +23,7 @@ public class MainActivity extends OpportunityFundBaseActivity implements Adapter
     private OpportunityFundBaseActivity mInstance;
     private Spinner spinner1, spinner2;
     private EditText mLoanAmount, mTerm, mDownPayment, mLoanFee, mRate;
+    private Button mCalcButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,9 @@ public class MainActivity extends OpportunityFundBaseActivity implements Adapter
         mLoanFee = (EditText) findViewById(R.id.input_loan_fee);
         mRate = (EditText) findViewById(R.id.input_rate);
 
+        mCalcButton = (Button) findViewById(R.id.btn_signup);
+
+        mCalcButton.setOnClickListener(this);
 
 
         spinner1.setOnItemSelectedListener(this);
@@ -64,13 +69,15 @@ public class MainActivity extends OpportunityFundBaseActivity implements Adapter
                 try {
                     jsonObject.put("requested_loan", mLoanAmount.getText().toString().trim());
                     jsonObject.put("months", mTerm.getText().toString().trim());
-                    jsonObject.put("downpayment_loan", mDownPayment.getText().toString().trim());
+                    jsonObject.put("downpayment_%", mDownPayment.getText().toString().trim());
                     jsonObject.put("loan_fee", mLoanFee.getText().toString().trim());
                     jsonObject.put("interest_rate", mRate.getText().toString().trim());
-                    jsonObject.put("loan_use", spinner1.getSelectedItem().toString().trim());
-                    jsonObject.put("frequency", spinner2.getSelectedItem().toString().trim());
+                    jsonObject.put("loan_use", spinner1.getSelectedItem().toString().toLowerCase().trim());
+                    jsonObject.put("frequency", spinner2.getSelectedItem().toString().toLowerCase().trim());
 
-                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "http://192.168.89.211:5000/calculate", new Response.Listener<JSONObject>() {
+                    Log.e("JSON", jsonObject.toString());
+
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://192.168.89.211:5000/calculate", jsonObject, new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
                             Log.e("Volley",response.toString());
@@ -80,6 +87,7 @@ public class MainActivity extends OpportunityFundBaseActivity implements Adapter
                         @Override
                         public void onErrorResponse(VolleyError error) {
                             // TODO Auto-generated method stub
+                            Log.e("Volley",error.toString());
                             VolleyLog.e(TAG, error);
                         }
                     });
