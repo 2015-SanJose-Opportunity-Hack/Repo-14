3312e7e5cde9ca8a9802215270ff8142b6e797e3
Vipul Kanade <vipultechjoy@gmail.com>
2015-10-04 11:18:50 -0700

    Resolved Errors

--- MobileApp/OpportunityFund/app/src/main/java/com/opohack/opportunityfund/paypal/model/DataModelManager.java
+++ MobileApp/OpportunityFund/app/src/main/java/com/opohack/opportunityfund/paypal/model/DataModelManager.java
@@ -1,8 +1,5 @@
 package com.opohack.opportunityfund.paypal.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by vipulkanade on 5/13/15.
  */
@@ -11,11 +8,7 @@ public class DataModelManager {
     private static DataModelManager mInstance;
     private static final Object obj = new Object();
 
-    private List<EmployeeInStore> mEmployeeInStoreList = new ArrayList<EmployeeInStore>();
 
-    private List<GetProductHistory> mGetProductHistoryList = new ArrayList<GetProductHistory>();
-
-    private List<GetProduct> mGetProductList = new ArrayList<GetProduct>();
 
     public DataModelManager() {
     }
@@ -28,27 +21,5 @@ public class DataModelManager {
         return mInstance;
     }
 
-    public List<EmployeeInStore> getEmployeeInStoreList() {
-        return mEmployeeInStoreList;
-    }
-
-    public void setEmployeeInStoreList(List<EmployeeInStore> mEmployeeInStoreList) {
-        this.mEmployeeInStoreList = mEmployeeInStoreList;
-    }
-
-    public List<GetProductHistory> getGetProductHistoryList() {
-        return mGetProductHistoryList;
-    }
-
-    public void setGetProductHistoryList(List<GetProductHistory> mGetProductHistoryList) {
-        this.mGetProductHistoryList = mGetProductHistoryList;
-    }
-
-    public List<GetProduct> getGetProductList() {
-        return mGetProductList;
-    }
-
-    public void setGetProductList(List<GetProduct> mGetProductList) {
-        this.mGetProductList = mGetProductList;
-    }
+  
 }
