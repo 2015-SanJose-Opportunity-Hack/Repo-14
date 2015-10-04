a33ff0efcd4c1ba1c98d6289a7beef996f473223
Vipul Kanade <vipultechjoy@gmail.com>
2015-10-04 01:00:22 -0700

    Added Constant Interface

---
+++ MobileApp/OpportunityFund/app/src/main/java/com/opohack/opportunityfund/paypal/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.opohack.opportunityfund.paypal.constants;
+
+/**
+ * Created by vipulkanade on 10/4/15.
+ */
+public interface Constants {
+
+    // JSON Response basic keys
+    String STATUS			= "Status";
+    String MESSAGE			= "Message";
+    String ERROR_LIST		= "ErrorList";
+}
--- MobileApp/OpportunityFund/app/src/main/java/com/opohack/opportunityfund/paypal/view/MainActivity.java
+++ MobileApp/OpportunityFund/app/src/main/java/com/opohack/opportunityfund/paypal/view/MainActivity.java
@@ -11,5 +11,5 @@ public class MainActivity extends OpportunityFundBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
 }
